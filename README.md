1b5836ee8e7b545b948851a320a248c87e6752b0
curtjenk <curtjenk@comcast.net>
2016-05-11 19:26:02 -0400

    added dependencies for angular ui calendar

--- README.md
+++ README.md
@@ -1,5 +1,62 @@
 # Ticket Fix
 
-## Ticketing system App
-Team: Curtis and Josh
+Ticket Fix provides a convienient way for apartment dwellers to request maintenance for their apartment.
 
+### Authors
+- Josh
+- Curtis
+
+The app has the following features
+- Registration
+- Login
+- Order
+- Session management
+- Credit Card payment processing (sorta!)
+
+### Tech
+
+DC Roasters uses a number of open source projects to work properly:
+
+* [AngularJS] - HTML enhanced for web apps!
+* [Twitter Bootstrap] - great UI boilerplate for modern web apps
+* [node.js] - evented I/O for the backend
+* [Express] - fast node.js network app framework
+* [jQuery] - duh
+* [Mongo] - noSql data store
+* [Mongoose] - object modeling for node.js
+* [Stripe] - for credit card processing
+
+### Installation
+  - cd to the "front" directory
+    * run npm install
+    * Note: uses port 8200
+  - cd to the "back" directory
+    * run npm install
+    * Note: uses port 3200
+
+### Demo
+
+http://curtisjenkins.net:8200
+
+### Todos
+ - Write Tests
+ - help
+
+### License
+
+MIT
+
+
+**Free Software, Well Yeah!**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+
+   [Mongo]: <http://mongodb.com>
+   [Mongoose]: <http://mongoosejs.com>
+   [node.js]: <http://nodejs.org>
+   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+   [express]: <http://expressjs.com>
+   [AngularJS]: <http://angularjs.org>
+   [jQuery]: <http://jquery.com>
+   [Stripe]: <http://stripe.com>
--- app/app.module.js
+++ app/app.module.js
@@ -1,41 +1,38 @@
-var recipeApp = angular.module('recipeApp', ['ngRoute', 'LocalStorageModule']);
+var ticketFixApp = angular.module('ticketFixApp', ['ngRoute', 'mwl.calendar']);
 
+ticketFixApp.config(['$compileProvider', function ($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}]);
 //"run" executes once all modules have been loaded.
-recipeApp.run( function($rootScope, $location, sharedData) {
+ticketFixApp.run( function($rootScope, $location) {
 
    $rootScope.$watch(
     //This is the watch value function. It should return the value which is being watched.
     //In this case, we're watching the path.
-    function() { 
-      return $location.path(); 
+    function() {
+      return $location.path();
     },
-    //This is the watch listener function.  It will should "do" something when the 
-    //watched value has changed.  In this case, if the path changed to something 
+    //This is the watch listener function.  It will should "do" something when the
+    //watched value has changed.  In this case, if the path changed to something
     //other than the home page and the user is not logged in pushed them to the home page.
-    function(a){  
-        
+    function(a){
+
         if(a !== '/'){
             console.log('url has changed : ' + a);
             //Put some code in here to check if they are logged in!
             //If not... send them home.
-            if (!sharedData.isLoggedIn()) {
-                console.log(' ... but the user is not logged in');
-                 $location.path('/');
-            }
+            // if (!sharedData.isLoggedIn()) {
+            //     console.log(' ... but the user is not logged in');
+            //      $location.path('/');
+            // }
         }
     });
 
 
 });
 
-recipeApp.directive('directive1', function() {
-    return {
-        controller: RightCtrl
-    };
-});
-
 //add  "check-image" (without quotes) attribute to the img element
-recipeApp.directive('checkImage', function($http) {
+ticketFixApp.directive('checkImage', function($http) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
@@ -50,4 +47,3 @@ recipeApp.directive('checkImage', function($http) {
         }
     };
 });
-
