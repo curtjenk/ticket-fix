dd349163c27532949ebd760d5526b78b9cc64b70
curtjenk <curtjenk@comcast.net>
2016-06-02 16:24:16 -0400

    Created "common" directory for resources shared between the front-end and back-end.

--- app/components/registration/registrationController.js
+++ app/components/registration/registrationController.js
@@ -1,4 +1,19 @@
 ticketFixApp.controller('registrationController', function($scope, $http) {
+var apiUrl = "http://localhost:3000";
 
-
+$scope.registerForm = function() {
+    var url = apiUrl + "/register";
+    var user = new User({
+      // type_user_id: 1,
+      email: $scope.email,
+      first_name: $scope.firstname,
+      password: $scope.password
+    });
+    $http.post(url, user).then(
+      function(success){
+        console.log(success);
+      }, function(err){
+        console.log(err);
+      });
+  };
 });
