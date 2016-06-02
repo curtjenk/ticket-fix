d47a88580c8f57956d60cf598d0adbac004d1588
jciarallij <jciarallij@gmail.com>
2016-06-02 17:14:29 -0400

    added few changes on registerController

--- app/components/registration/registrationController.js
+++ app/components/registration/registrationController.js
@@ -8,8 +8,13 @@ $scope.registerForm = function() {
       // type_user_id: 1,
       email: $scope.email,
       first_name: $scope.firstname,
-      password: $scope.password
+      last_name: $scope.lastname,
+      password: $scope.password,
+      property_code: $scope.propertycode,
+      home_phone: $scope.phone,
+      mobile_phone: $scope.mobile
     });
+    
     $http.post(url, user).then(
       function(success){
         console.log(success);
