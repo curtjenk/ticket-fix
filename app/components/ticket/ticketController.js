07e6f5689085e487f1634a80588926bb85645ae1
jciarallij <jciarallij@gmail.com>
2016-06-10 11:53:47 -0400

    no message

--- app/components/ticket/ticketController.js
+++ app/components/ticket/ticketController.js
@@ -1,12 +1,13 @@
 ticketFixApp.controller('ticketController', function ($rootScope, $scope, $http, $location, apiAjax, localStore) {
 	$scope.formData = {};
 
-	$scope.addMarker = function(event){
-		console.log('iasdhfkajsdhjfhdkajshkfdhksjhsadk')
-		console.log(event);
-		// var xClick = event.offsetX;
-		// var yClick = event.offsetY;
-		// $scope.newMarker = '<div class="marker" style="position: absolute; top: '+xClick+'; left: '+yClick+';">1</div>';
+	$scope.addMarker = function($event){
+		console.log('WOW YOU CLICKED IT!')
+		console.log($event);
+
+		var xClick = $event.offsetX;
+		var yClick = $event.offsetY;
+		$scope.newMarker = '<div class="marker" style="position: absolute; top: '+xClick+'; left: '+yClick+';">1</div>';
 	}
 
 
