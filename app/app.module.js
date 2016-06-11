61eeda1b1e421eb2e093bb669f7f214311892937
jciarallij <jciarallij@gmail.com>
2016-06-10 21:05:25 -0400

    ngSanitize added.

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
-var ticketFixApp = angular.module('ticketFixApp', ['ui.router', 'ui.bootstrap', 'ngMessages', 'LocalStorageModule', 'ngMaterial']);
+var ticketFixApp = angular.module('ticketFixApp', ['ui.router', 'ui.bootstrap', 'ngMessages', 'LocalStorageModule', 'ngMaterial','ngSanitize']);
 
 // ticketFixApp.config(['$compileProvider', function ($compileProvider) {
 //   $compileProvider.debugInfoEnabled(false);
