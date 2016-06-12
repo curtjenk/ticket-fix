9031232b10f95aab6d455482362c5650d5d1d4c1
curtjenk <curtjenk@comcast.net>
2016-06-12 12:37:15 -0400

    blah

--- app/components/manager/managerController.js
+++ app/components/manager/managerController.js
@@ -1,24 +1,39 @@
-ticketFixApp.controller('managerController', function($rootScope, $scope, $http, $q, $location, apiAjax, zipLookup) {
+ticketFixApp.controller('managerController', function ($rootScope, $scope, $http, $q, $location, apiAjax, zipLookup) {
+    $scope.formData = {};
+	var user = $rootScope.user;
+	console.log($rootScope.user);
+	var email = user.email;
 
-    var user = $rootScope.user;
-    console.log($rootScope.user);
-    var email = user.email;
+	apiAjax.getallmanagertickets(email).then(
+		function (succ) {
+			console.log(succ);
+			$scope.managerTickets = succ.data.info;
+		},
+		function (err) {
+			console.log(err);
+		});
 
-    apiAjax.getallmanagertickets(email).then(
-        function(succ) {
-            console.log(succ);
-            $scope.managerTickets = succ.data.info;
-        },
-        function(err) {
-            console.log(err);
-        });
-
-
-
-
-
-
-
-
-
+	/*
+	apiAjax.getallmanagerproperties(email).then(
+	    function(succ) {
+	        console.log(succ);
+	        $scope.managerProperties = succ.data.info;
+	    },
+	    function(err) {
+	        console.log(err);
+	    });
+	*/
+	$scope.zipLookup = function () {
+		if ($scope.formData.zip && $scope.formData.zip.length === 5 && is_int($scope.formData.zip.length)) {
+			var ok = function (resp) {
+				$scope.formData.city = resp.city;
+				$scope.formData.state = resp.state;
+			};
+			var error = function (err) {
+				$scope.formData.city = "";
+				$scope.formData.state = "";
+			};
+			zipLookup.get($scope.formData.zip, ok, error);
+		}
+	};
 });
