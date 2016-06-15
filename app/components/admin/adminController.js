1126b1a9722f78a8c1f80f18093ca6719fb9c9aa
curtjenk <curtjenk@comcast.net>
2016-06-15 15:43:07 -0400

    Added a refresh button for the ticket activity chart

--- app/components/admin/adminController.js
+++ app/components/admin/adminController.js
@@ -9,6 +9,8 @@ ticketFixApp.controller('adminController', function ($rootScope, $scope, $http, 
 	console.log($rootScope.user);
 	var email = user.email;
 
+	getTicketsPerDay();
+
 	apiAjax.getalltenantsinfo(email).then(
 		function (succ) {
 			console.log(succ);
@@ -18,30 +20,39 @@ ticketFixApp.controller('adminController', function ($rootScope, $scope, $http, 
 			console.log(err);
 		});
 
-	apiAjax.getTicketsPerDay(email).then(
-		function (succ) {
-			var ticksPerDay = succ.data.info;
-			console.log(succ);
-            $scope.series = ['Tickets'];
-            $scope.labels = [];
-            $scope.data =[];
-			for (var i = 0; i < ticksPerDay.length; i++) {
-                $scope.labels.push(ticksPerDay[i].DateOnly);
-                $scope.data.push(ticksPerDay[i].num);
-				// $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-				// $scope.series = ['Tickets'];
-				// $scope.data = [
-                //                                [65, 59, 80, 81, 56, 55, 40],
-                //                                [28, 48, 40, 19, 86, 27, 90]
-                //                            ];
-			}
-			$scope.onClick = function (points, evt) {
-				console.log(points, evt);
-			};
 
-		},
-		function (err) {
-			console.log(err);
-		});
+    $scope.refresh = function() {
+        getTicketsPerDay();
+    };
 
+    function getTicketsPerDay() {
+        $scope.labels = [];
+        $scope.data =[];
+        apiAjax.getTicketsPerDay(email).then(
+            function (succ) {
+                var ticksPerDay = succ.data.info;
+                console.log(succ);
+                $scope.series = ['Tickets'];
+
+                for (var i = 0; i < ticksPerDay.length; i++) {
+                    var d = moment(ticksPerDay[i].DateOnly);
+                    console.log(d.format('YYYY-MM-DD'));
+                    $scope.labels.push(d.format('YYYY-MM-DD'));
+                    $scope.data.push(ticksPerDay[i].num);
+                    // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+                    // $scope.series = ['Tickets'];
+                    // $scope.data = [
+                    //                                [65, 59, 80, 81, 56, 55, 40],
+                    //                                [28, 48, 40, 19, 86, 27, 90]
+                    //                            ];
+                }
+                $scope.onClick = function (points, evt) {
+                    console.log(points, evt);
+                };
+
+            },
+            function (err) {
+                console.log(err);
+            });
+    }
 });
