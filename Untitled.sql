1d807693d577014d3caf041ccb13cfc2b5ae6e5e
Curtis Jenkins <curtisjenkins@Curtiss-MacBook-Pro.local>
2016-07-30 17:56:11 -0400

    remove unused files

--- Untitled.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM ticketfix.type_status;
-
-insert into type_status (code, description) values('new','created but not yet assigned to work');
-insert into type_status (code, description) values('assigned','given to a contractor or internal staff to fix');
-insert into type_status (code, description) values('in-progress','started working on it');
-insert into type_status (code, description) values('blocked','something came up that work is on hold');
-insert into type_status (code, description) values('complete','issue resolved');
--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8000, function() {
-  console.log(' -------- Karm Yogis MyGrub -----------');
-  console.log('Directory = ' + __dirname);
-  console.log('Listening on port 8000...');
-
-});
